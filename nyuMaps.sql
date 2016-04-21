2a7334f50d863c23792866c36d653c9ef17c9d82
Kevin Li <kevinli159357@gmail.com>
2016-04-21 10:04:59 -0400

    Inserts for building location and modified tables

--- nyuMaps.sql
+++ nyuMaps.sql
@@ -3,6 +3,7 @@ create table users(
 	email varchar(40),
 	passwd  char(32),
 	pid SERIAL,
+	ign varchar(10)
 	PRIMARY KEY (pid)
 );
 
@@ -10,21 +11,23 @@ create table users(
 create table task(
 	taskID SERIAL,
 	taskToDo text,
-	PRIMARY KEY(taskid)
+	building varchar(20),
+	PRIMARY KEY(taskid),
+	foreign key(building) references building(name)
 );
 
 -- In game info (tile location etc)
 create table inGamePlayer(
 	ign varchar(10),
-	--taskLeft integer,
 	xcoord integer,
 	ycoord integer,
-	myTurn boolean,  		-- Not sure if needed yet, the room keep track of whose turn it is
-	taskID BIGINT ,   -- Problem here. Psql does not allow array of PK/FK   atm this only gives us 1 task
+	turnid integer,  	
+	roomid BIGINT,   
 	pid BIGINT,
 	PRIMARY KEY (pid),
 	FOREIGN KEY (pid) REFERENCES users(pid) ON DELETE CASCADE,
-	FOREIGN KEY (taskid) REFERENCES task(taskID)
+	foreign key (ign) references users(ign),
+	FOREIGN KEY (roomid) REFERENCES playerroom(roomID)
 );
 
 
@@ -32,15 +35,20 @@ create table inGamePlayer(
 create table playerRoom(
 	roomid SERIAL,
 	roomCount integer,
-	currentTurn integer,
 	startTime timestamp without time zone default (now() at time zone 'utc'),
 	endTime timestamp without time zone,
 	gameWinner BIGINT,
 	PRIMARY KEY (roomid),
-	FOREIGN KEY (currentTurn) REFERENCES users(pid),
 	FOREIGN KEY (gameWinner) REFERENCES users(pid)
 );
 
+-- table to keep track of current turn
+create table ingameroom(
+	roomid bigint,
+	currentturn integer,
+	primary key(roomid,currentturn),
+	foreign key(roomid) references playerroom(roomid)
+);
 -- table to check whether all players are ready
 create table inRoomStatus(
 	pid BIGINT,
@@ -52,36 +60,16 @@ create table inRoomStatus(
 );
 
 
--- Building (static info may merge with inGameBuilding) 
-create table building(
-	name varchar(30),
-	xcoord integer,
-	ycoord integer,
-	PRIMARY KEY (name)
---Needs building action/quantity 
-	
-);
+--buildings for ingame map generation
 create table inGameBuilding(
-	name varchar(30),
-	xcoord integer,
-	ycoord integer,
-	roomid BIGINT,
-	PRIMARY KEY (name,roomid),
-	FOREIGN KEY (roomid) REFERENCES playerRoom(roomid) ON DELETE CASCADE,
-	FOREIGN KEY (name) REFERENCES building(name)
-	
+	name varchar(20),
+	location text,
+	entrance varchar(5),
+	color varchar(12),
+	height varchar(5),
+	PRIMARY KEY (name),
 );
 
--- Resource (shared item amongst players? may be field of building...)
-create table resources(
-	name varchar(30),
-	roomid BIGINT,
-	xcoord integer,
-	ycoord integer,
-	PRIMARY KEY (name,roomid),
-	FOREIGN KEY (roomid) REFERENCES playerRoom(roomid) ON DELETE CASCADE
-	
-);
 
 
 
